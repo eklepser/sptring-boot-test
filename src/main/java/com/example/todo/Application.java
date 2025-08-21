c99f6788ea52bdaff2139519d292c616ab4d9f91
Nikitos <tishkovnikita2005@gmail.com>
2025-08-21 17:16:12 +0300

    Fixed properties files

--- src/main/java/com/example/todo/Application.java
+++ src/main/java/com/example/todo/Application.java
@@ -2,11 +2,10 @@ package com.example.todo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication()
+@EnableJpaRepositories
+@SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
